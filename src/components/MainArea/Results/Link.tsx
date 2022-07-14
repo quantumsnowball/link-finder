a71196d7dc019eacf7f815f04f9b0b754f814998
Quantum Snowball <quantum.snowball@gmail.com>
2022-07-14 20:00:20 +0800

    feature: show method as Chip; refactor: Link becomes styled component

--- src/components/MainArea/Results/Link.tsx
+++ src/components/MainArea/Results/Link.tsx
@@ -1,8 +1,9 @@
 import Highlighter from "react-highlight-words";
-import '../../../styles/Link.css'
 import { states } from '../../App'
 import { copyText } from '../../../utils/data'
 import { useContext } from 'react';
+import Paper from '@mui/material/Paper'
+import Chip from '@mui/material/Chip';
 
 
 interface LinkProps {
@@ -17,15 +18,29 @@ function Link({ url, title, method }: LinkProps) {
   } = useContext(states)
 
   return (
-    <p onClick={_ => copyText(
-      `youtube-dl "${url}" -o "${title}.mp4"`
-    )}>
-      <span>{method} </span>
+    <Paper
+      onClick={_ => copyText(
+        `youtube-dl "${url}" -o "${title}.mp4"`
+      )}
+      sx={{
+        '&:hover': { cursor: 'pointer' },
+        display: 'flex',
+        alignItems: 'center',
+        fontFamily: ['source-code-pro', 'monospace'],
+        wordBreak: 'break-all',
+        backgroundColor: 'background.paper',
+        margin: '0.2em',
+        padding: '0.2em',
+        fontSize: '1.2em'
+      }}
+      elevation={24}
+    >
+      <Chip label={method} variant="outlined" color="primary" size="small" />
       <Highlighter
         searchWords={[highlight]}
         textToHighlight={url}
       />
-    </p>
+    </Paper>
   )
 }
 
