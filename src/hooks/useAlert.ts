011957417859f0d9701a6aae6ad4bfcf97e26bcf
Quantum Snowball <quantum.snowball@gmail.com>
2022-07-14 23:55:27 +0800

    feature: only the latest timer can discard the alert popup

--- src/hooks/useAlert.ts
+++ src/hooks/useAlert.ts
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import { useState, useRef } from 'react'
 import { Alert } from '../types'
 
 
@@ -6,10 +6,23 @@ function useAlert(initialValue: Alert, timeout = 10000) {
   const [alert, setAlert] = useState(initialValue)
   const [alertMessage, setAlertMessage] = useState('')
 
+  // this ref holds the latest timerId
+  const idRef = useRef(0)
+
   const alertSuccess = (message: string) => {
+    // generate new id for every new timer
+    const timerId = Math.random()
+    // keep track of the latest timer id registered by Ref
+    idRef.current = timerId
+    // only allow setAlert to none if the timerId matches idRef
+    setTimeout(() => {
+      if (idRef.current === timerId) {
+        setAlert('none')
+      }
+    }, timeout)
+    // set the state of alert and message
     setAlert('success')
     setAlertMessage(message)
-    setTimeout(() => { setAlert('none') }, timeout);
   }
 
   return { alert, alertSuccess, alertMessage }
