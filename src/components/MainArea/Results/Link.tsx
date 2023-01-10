d72c259a2ba1ac607c50b5c4f68ff09f23aeecbc
Quantum Snowball <quantum.snowball@gmail.com>
2023-01-10 19:03:35 +0800

    feat: click to expand Link card to show extra info

--- src/components/MainArea/Results/Link.tsx
+++ src/components/MainArea/Results/Link.tsx
@@ -7,6 +7,8 @@ import { useSelector } from "react-redux"
 import { RootState } from "../../../redux/store"
 import useAlert from "../../../hooks/useAlert"
 import { useSelectProgram } from "../../../hooks/useSelect"
+import { Typography } from "@mui/material"
+import { useState } from "react"
 
 
 interface LinkProps {
@@ -20,41 +22,55 @@ function Link({ url, title, method, requestId }: LinkProps) {
   const { program } = useSelectProgram()
   const { alertSuccess, alertError } = useAlert()
   const highlight = useSelector((s: RootState) => s.input.highlight)
+  const [expanded, setExpanded] = useState(false)
 
   return (
-    <Paper
-      onClick={e => {
-        if (e.shiftKey)
-          copyText(url, alertSuccess, alertError)
-        else
-          copyText(`${program} "${url}" -o "${title}.mp4"`, alertSuccess, alertError)
-      }}
-      sx={{
-        '&:hover': { cursor: 'pointer' },
-        display: 'flex',
-        alignItems: 'center',
-        fontFamily: ['source-code-pro', 'monospace'],
-        wordBreak: 'break-all',
-        backgroundColor: 'background.paper',
-        margin: '0.2em',
-        padding: '0.2em',
-        fontSize: '1.2em',
-        borderRadius: 'shape.borderRadius',
-        userSelect: 'none'
-      }}
-      elevation={24}
-    >
-      <Chip
-        label={method}
-        color={methodChipStyle(method)}
-        size="small"
+    <Paper>
+      <Paper
+        onClick={e => {
+          if (e.shiftKey)
+            copyText(url, alertSuccess, alertError)
+          else
+            copyText(`${program} "${url}" -o "${title}.mp4"`, alertSuccess, alertError)
+          setExpanded(!expanded)
+        }}
         sx={{
-          marginRight: 1
-        }} />
-      <Highlighter
-        searchWords={[highlight]}
-        textToHighlight={url}
-      />
+          '&:hover': { cursor: 'pointer' },
+          display: 'flex',
+          alignItems: 'center',
+          fontFamily: ['source-code-pro', 'monospace'],
+          wordBreak: 'break-all',
+          backgroundColor: 'background.paper',
+          margin: '0.2em',
+          padding: '0.2em',
+          fontSize: '1.2em',
+          borderRadius: 'shape.borderRadius',
+          userSelect: 'none'
+        }}
+        elevation={24}
+      >
+        <Chip
+          label={method}
+          color={methodChipStyle(method)}
+          size="small"
+          sx={{
+            marginRight: 1
+          }} />
+        <Highlighter
+          searchWords={[highlight]}
+          textToHighlight={url}
+        />
+      </Paper>
+
+      {expanded ?
+        <Paper>
+          <Typography>
+            {requestId}: Some info about the response
+          </Typography>
+
+        </Paper>
+        : null
+      }
     </Paper>
   )
 }
