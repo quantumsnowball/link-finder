6ec482306d99de48042a6b953922c8598cf881d0
Quantum Snowball <quantum.snowball@gmail.com>
2023-01-11 14:29:06 +0800

    layout: topbar divided into 3 parts

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,21 +1,40 @@
 import {
   AppBar,
-  Button,
   IconButton,
-  Toolbar
+  Toolbar,
+  Box
 } from "@mui/material"
 import ClearIcon from '@mui/icons-material/Clear'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeIcon from '@mui/icons-material/DarkMode'
 
+
+const Left = () =>
+  <Box sx={{ display: "flex", flex: 1, justifyContent: 'flex-start' }}>
+    <IconButton >
+      <DarkModeIcon />
+    </IconButton >
+  </Box>
+
+const Middle = () =>
+  <Box sx={{ display: 'flex', flex: 1, justifyContent: 'center' }}>
+    Some title text
+  </Box>
+
+const Right = () =>
+  <Box sx={{ display: 'flex', flex: 1, justifyContent: 'flex-end' }}>
+    <IconButton >
+      <ClearIcon />
+    </IconButton>
+  </Box>
 
 const TopBar = () => {
   return (
     <AppBar position="static">
-      <Toolbar>
-        <IconButton
-          onClick={() => alert('Clear Button')}
-        >
-          <ClearIcon />
-        </IconButton>
+      <Toolbar sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Left />
+        <Middle />
+        <Right />
       </Toolbar>
     </AppBar>
   )
