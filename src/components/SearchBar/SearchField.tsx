ec7ec1f26b516cbb7fa2a6e34465d97a4c109954
Quantum Snowball <quantum.snowball@gmail.com>
2023-01-12 14:32:55 +0800

    feat: keyword presets logics

--- src/components/SearchBar/SearchField.tsx
+++ src/components/SearchBar/SearchField.tsx
@@ -1,6 +1,7 @@
 import { Link } from '@mui/material'
 import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
+import { useEffect, useState } from 'react'
 import { KEYWORD_PRESETS } from '../../constants'
 import { useRegex } from '../../hooks/generic/useRegex'
 
@@ -12,7 +13,22 @@ interface SearchFieldProps {
 }
 
 function SearchField({ label, value, setValue }: SearchFieldProps) {
+  const [display, setDisplay] = useState(value)
   const { isValidRegex, setValidRegex } = useRegex(setValue)
+
+  const appendKeyword = (kw: string) => {
+    if (value.length === 0)
+      setDisplay(kw)
+    else
+      setDisplay(value + '|' + kw)
+    console.log({ kw })
+  }
+
+  // test display value for valid regex on changes, 
+  // only set redux states if it is valid
+  // will keep the invalid regex in text field for editing
+  // error red box means redux states is not set and filter is not updated
+  useEffect(() => { setValidRegex(display) }, [display])
 
   return (
     <Box sx={{
@@ -25,8 +41,8 @@ function SearchField({ label, value, setValue }: SearchFieldProps) {
         margin="normal"
         label={label}
         error={!isValidRegex}
-        onChange={e => setValidRegex(e.target.value)}
-        defaultValue={value}
+        onChange={e => setDisplay(e.target.value)}
+        value={display}
         sx={{ m: 0 }}
       />
       <Box
@@ -38,7 +54,7 @@ function SearchField({ label, value, setValue }: SearchFieldProps) {
         {KEYWORD_PRESETS.map(kw =>
           <Link
             component="button"
-            onClick={() => alert('inject some text to textfield')}
+            onClick={() => appendKeyword(kw)}
             sx={{ mr: 1 }}
           >
             {kw}
