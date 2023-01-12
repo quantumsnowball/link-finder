cb8a4e18dd9607ed72716a2647f2775280e0cee6
Quantum Snowball <quantum.snowball@gmail.com>
2023-01-12 18:16:59 +0800

    layout: click to expand to show headers

---
+++ src/components/MainArea/Results/Card/Details/headers.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material"
+
+
+const Headers = () => {
+  return (
+    <Box>
+      Gonna show headers if details section is expanded
+    </Box>
+  )
+}
+
+export default Headers
--- src/components/MainArea/Results/Card/Details/index.tsx
+++ src/components/MainArea/Results/Card/Details/index.tsx
@@ -1,7 +1,8 @@
 import { Typography } from "@mui/material"
-import { FC } from "react"
+import { FC, useState } from "react"
 import { Box } from "@mui/system"
 import { Response } from "../../../../../types"
+import Headers from "./headers"
 
 
 type Props = {
@@ -10,16 +11,27 @@ type Props = {
 
 const Details: FC<Props> = ({ response }) => {
   const { title, requestId, statusLine } = response
+  const [expanded, setExpanded] = useState(false)
 
   return (
-    <Box sx={{ display: "flex", alignItems: "center" }} >
-      <Box sx={{ display: "flex", flex: 1, justifyContent: 'flex-start' }}>
-        <Typography variant="body2"> {requestId} </Typography></Box>
-      <Box sx={{ display: "flex", flex: 8, justifyContent: 'center' }}>
-        <Typography variant="h6"> {title} </Typography></Box>
-      <Box sx={{ display: "flex", flex: 1, justifyContent: 'flex-end' }}>
-        <Typography variant="body2"> {statusLine} </Typography></Box>
-    </Box>
+    <>
+      <Box
+        onClick={() => setExpanded(!expanded)}
+        sx={{
+          '&:hover': { cursor: 'pointer' },
+          display: "flex",
+          alignItems: "center"
+        }}
+      >
+        <Box sx={{ display: "flex", flex: 1, justifyContent: 'flex-start' }}>
+          <Typography variant="body2"> {requestId} </Typography></Box>
+        <Box sx={{ display: "flex", flex: 8, justifyContent: 'center' }}>
+          <Typography variant="h6"> {title} </Typography></Box>
+        <Box sx={{ display: "flex", flex: 1, justifyContent: 'flex-end' }}>
+          <Typography variant="body2"> {statusLine} </Typography></Box>
+      </Box>
+      {expanded ? <Headers /> : null}
+    </>
   )
 }
 
