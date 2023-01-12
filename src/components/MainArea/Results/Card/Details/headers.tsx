7e4a8876f678477d966b2a841f4e791d6740010f
Quantum Snowball <quantum.snowball@gmail.com>
2023-01-12 19:40:21 +0800

    layout: headers pretty print

--- src/components/MainArea/Results/Card/Details/headers.tsx
+++ src/components/MainArea/Results/Card/Details/headers.tsx
@@ -1,14 +1,57 @@
-import { Box } from "@mui/material"
+import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
 import { FC } from "react"
 import { Request, Response } from "../../../../../types"
 
 
-type Props = {
+type RendererProps = {
+  title: string
+  headers: chrome.webRequest.HttpHeader[] | undefined
+}
+const Renderer: FC<RendererProps> =
+  ({ title, headers }) =>
+    <TableContainer>
+      <Table size='small' padding='none'>
+        <TableHead>
+          <TableRow>
+            <TableCell
+              align='center'
+              colSpan={2}
+              sx={{
+                fontWeight: 'bold',
+              }}>{title}</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody >
+          {headers?.map(({ name, value }) =>
+            <TableRow>
+              <TableCell
+                component='code'
+                align='right'
+                sx={{
+                  fontSize: 12,
+                  fontWeight: 'bold',
+                  pr: 1
+                }}>{name}</TableCell>
+              <TableCell
+                component='code'
+                align='left'
+                sx={{
+                  fontSize: 12
+                }}>{value}</TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </TableContainer>
+
+
+
+type HeadersProps = {
   request: Request | undefined,
   response: Response
 }
 
-const Headers: FC<Props> = ({ request, response }) => {
+const Headers: FC<HeadersProps> = ({ request, response }) => {
   const requestHeaders = request?.requestHeaders
   const responseHeaders = response.responseHeaders
 
@@ -24,14 +67,14 @@ const Headers: FC<Props> = ({ request, response }) => {
           flex: 1
         }}
       >
-        {requestHeaders?.map(h => <span>{h.name}:{h.value}</span>)}
+        <Renderer title='Requests Headers' headers={requestHeaders} />
       </Box>
       <Box
         sx={{
           flex: 1
         }}
       >
-        {responseHeaders?.map(h => <span>{h.name}:{h.value}</span>)}
+        <Renderer title='Response Headers' headers={responseHeaders} />
       </Box>
     </Box>
   )
