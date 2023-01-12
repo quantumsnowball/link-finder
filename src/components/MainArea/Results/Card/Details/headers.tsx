7bc8125b6a9f13cee7e8b12bfd88638b40c7a5f4
Quantum Snowball <quantum.snowball@gmail.com>
2023-01-12 18:22:44 +0800

    layout: headers

--- src/components/MainArea/Results/Card/Details/headers.tsx
+++ src/components/MainArea/Results/Card/Details/headers.tsx
@@ -3,8 +3,26 @@ import { Box } from "@mui/material"
 
 const Headers = () => {
   return (
-    <Box>
-      Gonna show headers if details section is expanded
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-between'
+      }}
+    >
+      <Box
+        sx={{
+          flex: 1
+        }}
+      >
+        Request Headers
+      </Box>
+      <Box
+        sx={{
+          flex: 1
+        }}
+      >
+        Response Headers
+      </Box>
     </Box>
   )
 }
