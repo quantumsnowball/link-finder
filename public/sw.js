301bb4fe94ceb287b45ccb4f39c8d5901410287c
Quantum Snowball <quantum.snowball@gmail.com>
2023-12-13 03:53:29 +0800

    layout: onclick initial windows size and position

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,17 @@
-chrome.action.onClicked.addListener(() => {
+chrome.action.onClicked.addListener(async () => {
+  const width = 1500
+  const height = 720
+  const currentWindow = await new Promise(resolve => { chrome.windows.getCurrent(resolve) })
+  const left = Math.round(currentWindow.left + (currentWindow.width - width) / 2)
+  const top = Math.round(currentWindow.top + (currentWindow.height - height) / 2)
   chrome.windows.create({
     type: "popup",
-    url: chrome.runtime.getURL('index.html')
+    url: chrome.runtime.getURL('index.html'),
+    width,
+    height,
+    left,
+    top,
   })
 })
+
 
