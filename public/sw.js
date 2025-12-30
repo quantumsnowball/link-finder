bf9f8061bcf28150e062a4686f076526cbc5bddb
Quantum Snowball <quantum.snowball@gmail.com>
2025-12-30 22:07:27 +0800

    better start up size and position

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,23 @@
 chrome.action.onClicked.addListener(() => {
+  // Define screen dimensions
+  const screenWidth = 2560;
+  const screenHeight = 1080;
+
+  // Define popup window size
+  const popupWidth = 1024;
+  const popupHeight = 768;
+
+  // Calculate the position to center the popup
+  const left = (screenWidth / 2) - (popupWidth / 2);
+  const top = (screenHeight / 2) - (popupHeight / 2);
+
+  // Create window
   chrome.windows.create({
     type: "popup",
-    url: chrome.runtime.getURL('index.html')
-  })
-})
-
+    url: chrome.runtime.getURL("index.html"),
+    width: popupWidth, // Set the width of the popup
+    height: popupHeight, // Set the height of the popup
+    left: left, // Set the horizontal position (distance from the left of the screen)
+    top: top, // Set the vertical position (distance from the top of the screen)
+  });
+});
